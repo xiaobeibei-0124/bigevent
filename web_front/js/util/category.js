d05777262c611584df6577cca59fd98f80a1f208
123456 <1642229324@qq.com>
2020-02-10 21:24:54 +0800

    列表页热门排行完成

--- web_front/js/util/category.js
+++ web_front/js/util/category.js
@@ -1,29 +1,11 @@
 // 把所有与文章分类相关的操作写在这里
 var category = {
   // 获取
-  get: function() {
-    return $.get(APILIST.category_get)
-  },
+  get: () => $.get(APILIST.category_get),
   // 添加
-  add: function(name, slug) {
-    return $.post(APILIST.category_add, {
-      "name": name,
-      "slug": slug
-    })
-  },
+  add: (name, slug) => $.post(APILIST.category_add, { name, slug }),
   // 删除
-  del: function(id) {
-    return $.post(APILIST.category_del, {
-      "id": id
-    })
-  },
+  del: (id) => $.post(APILIST.category_del, { id }),
   // 编辑
-  edit: function(id, name, slug) {
-    return $.post(APILIST.category_edit, {
-      "id": id,
-      "name": name,
-      "slug": slug
-    })
-  },
-
+  edit: (id, name, slug) => $.post(APILIST.category_edit, { id, name, slug }),
 }
--- web_front/js/util/config.js
+++ web_front/js/util/config.js
@@ -2,10 +2,10 @@
 // 把项目中所有用到的接口地址，全部放在这里
 
 // 所有接口的及地址
-var BASEURL = "http://192.168.1.104:8000"
+const BASEURL = "http://192.168.1.104:8000"
 
 // 用来保存所有的接口信息
-var APILIST = {
+const APILIST = {
   // 文章类型相关接口
   // 获取文章类型
   category_get: BASEURL + "/category",
